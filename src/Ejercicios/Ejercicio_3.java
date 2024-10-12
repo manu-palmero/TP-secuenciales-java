39fa3354417676e8b9205ad56e873ff40b07d8a8
Manuel Palmero <manueligna06@gmail.com>
2024-10-12 04:29:33 -0300

    Primer confirmación

---
+++ src/Ejercicios/Ejercicio_3.java
@@ -0,0 +1,35 @@
+package Ejercicios;
+
+import javax.swing.*;
+
+/*
+Ejercicio 3: Generador de Rutinas de Ejercicio
+Escribe un programa que pida al usuario su nivel de condición física (principiante, intermedio, avanzado)
+y luego genere una rutina de ejercicio semanal con diferentes tipos de ejercicios y
+duraciones.
+*/
+
+
+public class Ejercicio_3 {
+    public static void main(String[] args) {
+        ej3();
+    }
+
+    public static void ej3() {
+        String[] condiciones = {
+                "Principiante",
+                "Intermedio",
+                "Avanzado"
+        };
+        String condicion = (String) JOptionPane.showInputDialog(
+                null,
+                "Seleccione qué nivel de condición física tiene:",
+                "Condición Física",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                condiciones,
+                null
+        );
+
+    }
+}
---
+++ src/Secuenciales.java
@@ -0,0 +1,9 @@
+import Ejercicios.*;
+
+public class Secuenciales {
+    public static void main(String[] args) {
+        Ejercicio_1.ej1();
+        Ejercicio_2.ej2();
+        Ejercicio_3.ej3();
+    }
+}
